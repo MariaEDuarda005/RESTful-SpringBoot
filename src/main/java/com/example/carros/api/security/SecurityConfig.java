b5ea6492df204c9848b7295392c45a841e0868ba
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-07-16 14:24:00 -0300

    Memory Authentication e Perfil de acesso (Roles)

--- src/main/java/com/example/carros/api/security/SecurityConfig.java
+++ src/main/java/com/example/carros/api/security/SecurityConfig.java
@@ -1,66 +1,52 @@
 package com.example.carros.api.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true) // habilitando a segurança por metodo
 public class SecurityConfig {
-
-    @Autowired
-    @Qualifier("userDetailsService")
-    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorize -> authorize
-                        .anyRequest().authenticated()//.permitAll()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Permitir todas as requisições
                 )
-                .httpBasic(withDefaults());
-                // .csrf(csrf -> csrf.disable()); // Utilize lambda para desativar CSRF
-
-        // Para a configuração padrão, você pode usar:
-        // http.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
-        //     .httpBasic(withDefaults())
-        //     .csrf().disable();
+                .sessionManagement(session -> session
+                        // garante que cada requisição seja tratada independentemente, sem depender de sessões HTTP.
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
 
         return http.build();
     }
-//
-//    @Autowired
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 
-    // @Bean
-    // public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-    //     UserDetails user = User.withUsername("user")
-    //         .password(passwordEncoder().encode("user"))
-    //         .roles("USER")
-    //         .build();
-    //     UserDetails admin = User.withUsername("admin")
-    //         .password(passwordEncoder().encode("admin"))
-    //         .roles("USER", "ADMIN")
-    //         .build();
-    //     return new InMemoryUserDetailsManager(user, admin);
-    // }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        // dois usuarios com perfil diferente
+
+        auth
+                .inMemoryAuthentication().passwordEncoder(encoder)
+                .withUser("user").password(encoder.encode("user")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
+    }
 }
